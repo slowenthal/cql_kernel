ec331929e874cb7d247d4146567b8792b4d96e3f
stevelowenthal <steve@datastax.com>
2016-01-23 20:57:17 +0100

    start writing a couple of tests

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author='Thomas Kluyver / Steven Lowenthal',
     author_email='steve@datastax.com',
     install_requires = ['cassandra-driver>=2.7.2, <3.0'],
-    tests_require = ['jupyter_kernel_test>=0.1','nose>=1.3.7'],
+    tests_require = ['jupyter_kernel_test','nose>=1.3.7'],
     description=''
 )
