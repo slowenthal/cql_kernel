cc40b48f71845ac1df040cbfd0e3dade74d43366
stevelowenthal <steve@datastax.com>
2016-01-23 16:26:26 +0100

    Setup dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license='',
     author='Thomas Kluyver / Steven Lowenthal',
     author_email='steve@datastax.com',
-    install_requires = ['cassandra-driver>=2.7.2'],
+    install_requires = ['cassandra-driver>=2.7.2, <3.0'],
     tests_require = ['jupyter_kernel_test>=0.1','nose>=1.3.7'],
     description=''
 )
