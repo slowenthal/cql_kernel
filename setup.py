38f8334b5d4dc073e93c77aaad5c4395ab1e3fac
stevelowenthal <steve@datastax.com>
2016-01-23 15:56:17 +0100

    Finally get colors working

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(
     license='',
     author='Thomas Kluyver / Steven Lowenthal',
     author_email='steve@datastax.com',
+    install_requires = ['cassandra-driver>=2.7.2'],
+    tests_require = ['cassandra-driver>=2.7.2','nose>=1.3.7'],
     description=''
 )
