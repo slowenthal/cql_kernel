574269d9254f00bddeee42a59830ceac34ab08d4
stevelowenthal <steve@datastax.com>
2016-02-24 21:52:33 -0800

    fix dependency

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='cql_kernel',
-    version='0.2.2',
+    version='0.2.3',
     packages=['cqlshlib_cql_kernel', 'cqlshlib_cql_kernel.test', 'cql_kernel'],
     url='https://github.com/slowenthal/cql_kernel',
     license='Apache',
     author='Steven Lowenthal & Apache Cassandra Developers',
     author_email='steve@datastax.com',
-    install_requires = ['cassandra-driver>=2.7.2, <3.0'],
+    install_requires = ['cassandra-driver>=2.7.2,<3.0'],
     tests_require = ['jupyter_kernel_test','nose>=1.3.7'],
     description='CQL Kernel for Jupyter based on cqlsh'
 )
