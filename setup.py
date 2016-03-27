6f8326889fd05f671a26feacc893b051479bd2ab
stevelowenthal <steve@datastax.com>
2016-03-27 12:13:59 -0700

    Get cstar3 working

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 
 setup(
     name='cql_kernel',
-    version='0.2.3',
+    version='3.0.1',
     packages=['cqlshlib_cql_kernel', 'cqlshlib_cql_kernel.test', 'cql_kernel'],
     url='https://github.com/slowenthal/cql_kernel',
     license='Apache',
     author='Steven Lowenthal & Apache Cassandra Developers',
     author_email='steve@datastax.com',
-    install_requires = ['cassandra-driver>=2.7.2,<3.0'],
     tests_require = ['jupyter_kernel_test','nose>=1.3.7'],
-    description='CQL Kernel for Jupyter based on cqlsh'
+    description='CQL Kernel for Jupyter based on cqlsh',
+    package_data= {
+        '':['lib/*.zip']
+    }
 )
