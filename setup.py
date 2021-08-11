b77cc059e69648654b7f30bd16359feda39d94b2
Brad Schoening <bschoeni@gmail.com>
2021-08-11 14:32:50 -0400

    added README.md as pypi long_description

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    readme = f.read()
+
 setup(
     name='cqljupyter',
-    version='0.9.8',
+    version='0.9.86',
     packages=['cqlshlib', 'cqlshlib.test', 'cqljupyter'],
     url='https://github.com/bschoening/cqljupyter',
     license='Apache',
     author='Brad Schoening, Steven Lowenthal & Apache Cassandra Developers',
     install_requires = ['cassandra-driver>=3.25'],
-    tests_require = ['jupyter_kernel_test','nose>=1.3.7'],
+    tests_require = ['jupyter_kernel_test'],
     description='CQL kernel for Jupyter based on Cassandra CQLSH',
-    long_description='CQL kernel for Jupyter based on Cassandra 3.11.x CQLSH',
+    # long_description='CQL kernel for Jupyter based on Cassandra 3.11.x CQLSH',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" 
         'Intended Audience :: Developers',      # Define that your audience are developers
